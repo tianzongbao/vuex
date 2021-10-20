0a132d3d8678abef0730a75d2e285df0d4b51724
tianzongbao <2302328527@qq.com>
2021-10-20 15:10:10 +0800

    第一次提交代码

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+import store from './store/index.js'
+const incr = ()=>{
+    store.commit('INCREMENT');
+};
+const deincr = ()=>{
+    store.commit('DEINCREMENT');
+};
+</script>
+<template>
+    <div>
+        <h2>当前值：{{store.state.count}}</h2>
+        <button @click="incr">增加</button>
+</template>
+
